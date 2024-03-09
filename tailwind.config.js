cb4e9832e5b5666c4d76c3bee5ace537970a2a04
Hamze Mohamed <hamzemohamed9412@gmail.com>
2024-03-10 01:02:52 +0200

    project setup with tailwin css

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,jsx}"], // it means all html and js files inside src folder
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
